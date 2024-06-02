7798c2d1f95d3d26ea7ce0e4d681310514e26c90
meya <milanela.milanova@gmail.com>
2024-06-02 21:05:03 +0200

    third commit

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,31 +3,31 @@ import { ImageSource, Sound, Resource, Loader, } from 'excalibur'
 import { TiledResource } from '@excaliburjs/plugin-tiled'
 
 const Resources = {
-    IntroOneBackground: new ImageSource('src/used/img/IntroBG1.png'),
-    IntroTwoBackground: new ImageSource('src/used/img/IntroBG2.png'),
-    IntroThreeBackground: new ImageSource('src/used/img/IntroBG3.png'),
-    IntroFourBackground: new ImageSource('src/used/img/IntroBG4.png'),
+    IntroOneBackground: new ImageSource('img/IntroBG1.png'),
+    IntroTwoBackground: new ImageSource('img/IntroBG2.png'),
+    IntroThreeBackground: new ImageSource('img/IntroBG3.png'),
+    IntroFourBackground: new ImageSource('img/IntroBG4.png'),
 
-    MapOne: new TiledResource('src/used/maps/Floor1.0.tmx'),
-    MapTwo: new TiledResource('src/used/maps/Floor2.1.tmx'),
-    MapThree: new TiledResource('src/used/maps/Floor3.0.tmx'),
-    MapFour: new TiledResource('src/used/maps/Floor4.0.tmx'),
+    MapOne: new TiledResource('maps/Floor1.0.tmx'),
+    MapTwo: new TiledResource('maps/Floor2.1.tmx'),
+    MapThree: new TiledResource('maps/Floor3.0.tmx'),
+    MapFour: new TiledResource('maps/Floor4.0.tmx'),
 
-    MainChar: new ImageSource('src/used/char/MainChar-sheet1.png'),
-    Kuker: new ImageSource('src/used/char/Kuker.png'),
-    SamodivaBad: new ImageSource('src/used/char/Samodivas.png'),
-    Fiut: new ImageSource('src/used/char/Fiut.png'),
-    BabaYaga: new ImageSource('src/used/char/babaYaga.png'),
+    MainChar: new ImageSource('char/MainChar-sheet1.png'),
+    Kuker: new ImageSource('char/Kuker.png'),
+    SamodivaBad: new ImageSource('char/Samodivas.png'),
+    Fiut: new ImageSource('char/Fiut.png'),
+    BabaYaga: new ImageSource('char/babaYaga.png'),
 
-    OutroWon: new ImageSource('src/used/img/won.png'),
-    OutroLost: new ImageSource('src/used/img/lost.png'),
+    OutroWon: new ImageSource('img/won.png'),
+    OutroLost: new ImageSource('img/lost.png'),
 
-    KukerText: new ImageSource('src/used/img/kuker-text.png'),
-    FiutText: new ImageSource('src/used/img/fiut-text.png'),
-    SamodivaText: new ImageSource('src/used/img/samodiva-text.png'),
-    BabaYagaText: new ImageSource('src/used/img/babaYaga-text.png'),
-    EnterDoorSticker: new ImageSource ('src/used/img/enterDoorSticker.png'),
-    Key: new ImageSource('src/used/img/key.png')
+    KukerText: new ImageSource('img/kuker-text.png'),
+    FiutText: new ImageSource('img/fiut-text.png'),
+    SamodivaText: new ImageSource('img/samodiva-text.png'),
+    BabaYagaText: new ImageSource('img/babaYaga-text.png'),
+    EnterDoorSticker: new ImageSource ('img/enterDoorSticker.png'),
+    Key: new ImageSource('img/key.png')
 };
 
 const ResourceLoader = new Loader();
